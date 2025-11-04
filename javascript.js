6d62a9d6a2a1278a03b97974e72a26b7a669e5ad
Your Name <you@example.com>
2025-11-04 14:13:08 -0300

    feat: Modificações no projeto adicionando a nova consultória

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
-tailwind.config = {
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Inter', 'sans-serif'],
-                serif: ['Roboto', 'serif'],
-            },
+ tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                        serif: ['Roboto', 'serif'],
+                    },
+                }
+            }
         }
-    }
-}
 
-function toggleMenu() {
-    const menu = document.getElementById('mobile-menu');
-    const button = document.getElementById('menu-button');
-    menu.classList.toggle('menu-open');
-    button.setAttribute('aria-expanded', menu.classList.contains('menu-open'));
-}
+        function toggleMenu() {
+            const menu = document.getElementById('mobile-menu');
+            const button = document.getElementById('menu-button');
+            menu.classList.toggle('menu-open');
+            button.setAttribute('aria-expanded', menu.classList.contains('menu-open'));
+        }
